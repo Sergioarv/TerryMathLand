dd18566aafc4320edc350a2e60c8657a09395123
sergio rodriguez <sarv9208@hotmail.com>
2022-04-27 20:42:30 -0500

    Interaccion de dirreccion al tomar objetos

--- Assets/Scripts/ControladorPersonajeUp.cs
+++ Assets/Scripts/ControladorPersonajeUp.cs
@@ -12,6 +12,9 @@ public class ControladorPersonajeUp : MonoBehaviour
 
     Vector2 direcccion;
 
+    public GameObject mano;
+    public Vector2 posMano;
+
     bool mirandoDerecha;
     float movX;
     float movY;
@@ -22,6 +25,7 @@ public class ControladorPersonajeUp : MonoBehaviour
         animatorPlayer = GetComponent<Animator>();
         spritePlayer = GetComponent<SpriteRenderer>();
         mirandoDerecha = spritePlayer.flipX;
+        posMano = mano.transform.position;
     }
 
     private void Update()
@@ -32,10 +36,25 @@ public class ControladorPersonajeUp : MonoBehaviour
         animatorPlayer.SetFloat("MovX", movX);
         animatorPlayer.SetFloat("MovY", movY);
 
+
+        spritePlayer.sortingOrder = animatorPlayer.GetFloat("UltimoY") == 1 ? 1 : 0;
+
         if (movX != 0 || movY != 0)
         {
             animatorPlayer.SetFloat("UltimoX", movX);
             animatorPlayer.SetFloat("UltimoY", movY);
+
+            if (movX != 0)
+            {
+                float vX = movX * 0.3f;
+                mano.transform.localPosition = new Vector2(vX, 0);
+            }else if(movY !=0)
+            {
+                float vY = movY * 0.3f;
+                mano.transform.localPosition = new Vector2(0f, vY);
+            }
+
+            Debug.Log(mano.transform.localPosition);
         }
 
         direcccion = new Vector2(movX, movY).normalized;
--- Assets/Scripts/ManoJugador.cs
+++ Assets/Scripts/ManoJugador.cs
@@ -5,11 +5,56 @@ using UnityEngine;
 public class ManoJugador : MonoBehaviour
 {
 
+    public GameObject mano;
+    private GameObject objetoEnMano = null;
+    private GameObject objetoPadre;
+    private Vector2 posInicialObjeto;
+
+    private void Update()
+    {
+        if (objetoEnMano != null)
+        {
+            if (Input.GetKey(KeyCode.R))
+            {
+                objetoEnMano.transform.position = posInicialObjeto;
+                objetoEnMano.transform.SetParent(objetoPadre.transform);
+                //objetoEnMano.transform.position = new Vector2(objetoEnMano.transform.position.x, objetoEnMano.transform.position.y - 0.3f);
+                objetoEnMano = null;
+            }
+        }
+
+    }
+
+    /*
+     * private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Opcion"))
+        {
+
+            if (Input.GetKey(KeyCode.E) && objetoEnMano == null)
+            {
+                objetoPadre = collision.transform.parent.gameObject;
+                posInicialObjeto = collision.transform.position;
+                collision.transform.position = mano.transform.position;
+                collision.transform.SetParent(transform);
+                objetoEnMano = collision.gameObject;
+            }
+
+        }
+    }
+    */
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Opcion"))
         {
-            collision.transform.SetParent(transform);
+            if (Input.GetKey(KeyCode.E) && objetoEnMano == null)
+            {
+                objetoPadre = collision.transform.parent.gameObject;
+                posInicialObjeto = collision.transform.position;
+                collision.transform.position = mano.transform.position;
+                collision.transform.SetParent(transform);
+                objetoEnMano = collision.gameObject;
+            }
         }
     }
 }
